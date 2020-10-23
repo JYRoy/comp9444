36fa778334a0d8db8b8b63748a6972a4cabd90fd
jyroy <jyroooy@163.com>
2020-10-23 22:26:23 +0800

    update assn1.doxc

--- assignment1/hw1/spiral.py
+++ assignment1/hw1/spiral.py
@@ -52,30 +52,30 @@ class RawNet(torch.nn.Module):
         return output
 
 def graph_hidden(net, layer, node):
-    pass
+    # pass
     # plt.clf()
-    # xrange = torch.arange(start=-7, end=7.1, step=0.01, dtype=torch.float32)
-    # yrange = torch.arange(start=-6.6, end=6.7, step=0.01, dtype=torch.float32)
-    # xcoord = xrange.repeat(yrange.size()[0])
-    # ycoord = torch.repeat_interleave(yrange, xrange.size()[0], dim=0)
-    # grid = torch.cat((xcoord.unsqueeze(1), ycoord.unsqueeze(1)), 1)
-    #
-    # with torch.no_grad():  # suppress updating of gradients
-    #     net.eval()  # toggle batch norm, dropout
-    #     output = net(grid)
-    #     net.train()  # toggle batch norm, dropout back again
-    #
-    #     if layer == 1:
-    #         pred = (net.hidden_layer_1[:, node] >= 0.5).float()
-    #     else:
-    #         pred = (net.hidden_layer_2[:, node] >= 0.5).float()
-    #
-    #     #pred = (output >= 0.5).float()
-    #
-    #     # plot function computed by model
-    #     plt.clf()
-    #     plt.pcolormesh(xrange, yrange, pred.cpu().view(yrange.size()[0], xrange.size()[0]), shading='auto',
-    #                    cmap='Wistia')
+    xrange = torch.arange(start=-7, end=7.1, step=0.01, dtype=torch.float32)
+    yrange = torch.arange(start=-6.6, end=6.7, step=0.01, dtype=torch.float32)
+    xcoord = xrange.repeat(yrange.size()[0])
+    ycoord = torch.repeat_interleave(yrange, xrange.size()[0], dim=0)
+    grid = torch.cat((xcoord.unsqueeze(1), ycoord.unsqueeze(1)), 1)
+
+    with torch.no_grad():  # suppress updating of gradients
+        net.eval()  # toggle batch norm, dropout
+        output = net(grid)
+        net.train()  # toggle batch norm, dropout back again
+
+        if layer == 1:
+            pred = (net.hidden_layer_1[:, node] >= 0.5).float()
+        else:
+            pred = (net.hidden_layer_2[:, node] >= 0.5).float()
+
+        #pred = (output >= 0.5).float()
+
+        # plot function computed by model
+        plt.clf()
+        plt.pcolormesh(xrange, yrange, pred.cpu().view(yrange.size()[0], xrange.size()[0]), shading='auto',
+                       cmap='Wistia')
 
 
 
